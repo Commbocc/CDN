cd2dab358d7780734dd08253042c04b0aec0f593
Kevin McCarthy <kjmagic13@gmail.com>
2015-01-29 12:35:05 -0500

    added comments

--- js/highcharts/custom/issued-permits.js
+++ js/highcharts/custom/issued-permits.js
@@ -1,45 +1,49 @@
 $(document).ready(function () {
 
-		$('body').append("<style type=\"text/css\">pre.data { display: none; } .chart-pie-3d { min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto }</style>");
+	// functions
 
-		function chartSeriesData(csvData) {
-			var seriesData = [];
-			var lines = csvData.split(/\r|\r\n|\n/);
-			$.each(lines, function(index,line) {
-				var items = line.split(',');
-				if (items[0].length) {
-					seriesData.push({
-						name: items[0],
-						y:parseFloat(items[1])
-					});
-				};
-			});
-			return seriesData;
-		}
+	function chartSeriesData(csvData) {
+		var seriesData = [];
+		var lines = csvData.split(/\r|\r\n|\n/);
+		$.each(lines, function(index,line) {
+			var items = line.split(',');
+			if (items[0].length) {
+				seriesData.push({
+					name: items[0],
+					y:parseFloat(items[1])
+				});
+			};
+		});
+		return seriesData;
+	}
 
-		$('.chart-pie-3d').each(function(){
-			var options = {
-				chart: {
-					type: 'pie',
-					options3d: {
+	// execs
+
+	$('body').append("<style type=\"text/css\">pre.data { display: none; } .chart-pie-3d { min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto }</style>");
+
+	$('.chart-pie-3d').each(function(){
+		var options = {
+			chart: {
+				type: 'pie',
+				options3d: {
+					enabled: true,
+					alpha: 40
+				}
+			},
+			title: {
+				text: $(this).data('title') || null
+			},
+			tooltip: {
+				pointFormat: '{series.name}: <b>{point.y}</b> ({point.percentage:.1f}%)'
+			},
+			plotOptions: {
+				pie: {
+					innerSize: 100,
+					depth: 45,
+					allowPointSelect: true,
+					cursor: 'pointer',
+					dataLabels: {
 						enabled: true,
-						alpha: 40
-					}
-				},
-				title: {
-					text: $(this).data('title') || null
-				},
-				tooltip: {
-					pointFormat: '{series.name}: <b>{point.y}</b> ({point.percentage:.1f}%)'
-				},
-				plotOptions: {
-					pie: {
-						innerSize: 100,
-						depth: 45,
-						allowPointSelect: true,
-						cursor: 'pointer',
-						dataLabels: {
-							enabled: true,
 							// format: '<b>{point.name}</b>: {point.percentage:.1f} %'
 						},
 						showInLegend: false
@@ -58,62 +62,62 @@ $(document).ready(function () {
 		});
 
 
-		$('.chart-column-3d').each(function(){
-			var options = {
-				chart: {
-					type: 'column'
-				},
+	$('.chart-column-3d').each(function(){
+		var options = {
+			chart: {
+				type: 'column'
+			},
+			title: {
+				text: $(this).data('title') || null
+			},
+			xAxis: {
+				type: 'category',
 				title: {
-					text: $(this).data('title') || null
+					text: $(this).data('xaxis-label') || null
 				},
-				xAxis: {
-					type: 'category',
-					title: {
-						text: $(this).data('xaxis-label') || null
-					},
-					labels: {
-						rotation: -45,
-						style: {
-							fontSize: '13px',
-							fontFamily: 'Verdana, sans-serif'
-						}
+				labels: {
+					rotation: -45,
+					style: {
+						fontSize: '13px',
+						fontFamily: 'Verdana, sans-serif'
 					}
-				},
-				yAxis: {
-					min: 0,
-					title: {
-						text: $(this).data('yaxis-label') || null
+				}
+			},
+			yAxis: {
+				min: 0,
+				title: {
+					text: $(this).data('yaxis-label') || null
+				}
+			},
+			legend: {
+				enabled: false
+			},
+			tooltip: {
+				pointFormat: '<b>${point.y}</b>'
+			},
+			series: [{
+				name: 'Population',
+				dataLabels: {
+					enabled: true,
+					rotation: -90,
+					color: '#FFFFFF',
+					align: 'right',
+					x: 4,
+					y: 10,
+					style: {
+						fontSize: '13px',
+						fontFamily: 'Verdana, sans-serif',
+						textShadow: '0 0 3px black'
 					}
-				},
-				legend: {
-					enabled: false
-				},
-				tooltip: {
-					pointFormat: '<b>${point.y}</b>'
-				},
-				series: [{
-					name: 'Population',
-					dataLabels: {
-						enabled: true,
-						rotation: -90,
-						color: '#FFFFFF',
-						align: 'right',
-						x: 4,
-						y: 10,
-						style: {
-							fontSize: '13px',
-							fontFamily: 'Verdana, sans-serif',
-							textShadow: '0 0 3px black'
-						}
-					}
-				}]
-			};
-			var csvData = $(this, '.data').text().replace(/\t/g, '');
-			options.series.push({
-				data: chartSeriesData(csvData),
-				name: $(this).data('series-name') || 'Value'}
-				);
-			$(this).highcharts(options);
-		});
+				}
+			}]
+		};
+		var csvData = $(this, '.data').text().replace(/\t/g, '');
+		options.series.push({
+			data: chartSeriesData(csvData),
+			name: $(this).data('series-name') || 'Value'}
+			);
+		$(this).highcharts(options);
+	});
 
 });
