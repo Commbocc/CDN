be9db565f83483ca95f417a7f8feed83784bf5e3
unknown <McCarthyK@COM-IT132829D.hcbocc.ad>
2015-02-06 10:30:17 -0500

    google spreadsheet data with pop up pie

--- js/highcharts/custom/google-sheets.js
+++ js/highcharts/custom/google-sheets.js
@@ -11,6 +11,9 @@ $(document).ready(function () {
 		title: {
 			text: 'Issued Permits - All County'
 		},
+		subtitle: {
+			text: 'Click a data column for more information.'
+		},
 		credits: {
 			enabled: false
 		},
@@ -23,15 +26,21 @@ $(document).ready(function () {
 		{
 			type: 'column',
 			index: 0,
-			color: Highcharts.getOptions().colors[0]
+			color: Highcharts.getOptions().colors[0],
+			point: { events: { click: pointClick } },
+			cursor: 'pointer'
 		}, {
 			type: 'column',
 			index: 1,
-			color: Highcharts.getOptions().colors[2]
+			color: Highcharts.getOptions().colors[2],
+			point: { events: { click: pointClick } },
+			cursor: 'pointer'
 		}, {
 			type: 'column',
 			index: 2,
-			color: Highcharts.getOptions().colors[3]
+			color: Highcharts.getOptions().colors[3],
+			point: { events: { click: pointClick } },
+			cursor: 'pointer'
 		}, {
 			type: 'spline',
 			index: 3,
@@ -40,12 +49,7 @@ $(document).ready(function () {
 				lineWidth: 2,
 				lineColor: Highcharts.getOptions().colors[5],
 				fillColor: 'white'			
-			},
-			point: {
-				events: {
-					click: pointClick
-				}
-			},
+			}
 		}]
 	};
 
@@ -53,45 +57,95 @@ $(document).ready(function () {
 
 	var allData = Highcharts.data({
 		googleSpreadsheetKey: '1z-LA9Htodmj4G5Eq82myGKbaxElYEr_BC994PFsNujE',
-		googleSpreadsheetWorksheet: 3
-	});
+		googleSpreadsheetWorksheet: 1
+	});	
 
 	function pointClick() {
-		// console.log(this);
 		var $div = $('<div></div>').dialog({
 			title: this.name,
 			width: '60%',
 			height: 450
 		});
 
+		var dataColumn = allData.columns[this.index+3]
+
+		var eoMonth = this.x;
+		var eoMonthName = dataColumn[0];
+		var serieName = this.series.name;
+
+		var monthData = [];
+		for (var i = 0; i < dataColumn.length; i++) {
+			var category = allData.columns[0][i]
+			if ( category && dataColumn[i] && category.indexOf(serieName) > -1 ) {
+				monthData.push({name: allData.columns[2][i], y: dataColumn[i]});
+			};
+		};
+
 		window.chart = new Highcharts.Chart({
 			chart: {
 				renderTo: $div[0],
-				type: 'coulmn'
+				type: 'pie'
 			},
-			data: {
-				googleSpreadsheetKey: '1z-LA9Htodmj4G5Eq82myGKbaxElYEr_BC994PFsNujE',
-				googleSpreadsheetWorksheet: 2
-			}
-			// series: [{
-			// 	type: 'pie',
-			// 	name: 'Browser share',
-			// 	data: [
-			// 	['Firefox',   45.0],
-			// 	['IE',       26.8],
-			// 	{
-			// 		name: 'Chrome',
-			// 		y: 12.8,
-			// 		sliced: true,
-			// 		selected: true
-			// 	},
-			// 	['Safari',    8.5],
-			// 	['Opera',     6.2],
-			// 	['Others',   0.7]
-			// 	]
-			// }]
+			title: {
+				text: serieName + " - " + eoMonthName
+			},
+			tooltip: {
+				pointFormat: '{point.tooltip}{series.name}: <b>{point.y}</b> ({point.percentage:.1f}%)'
+			},
+			credits: {
+				enabled: false
+			},
+			series: [{
+				name: 'Permits',
+				data: minisculeValues(monthData)
+			}]
 		});
 
+	}
+
+	function minisculeValues(seriesData) {
+		var minThreshold = .035;
+		var miniValue = 0;
+		var miniLabel = "";
+		var total = 0;
+		var passedValues= [];
+		for (var i = 0; i < seriesData.length; i++) {
+			total += seriesData[i].y;
+		};
+
+		for (var i = 0; i < seriesData.length; i++) {
+			if ( seriesData[i].y/total <= minThreshold ) {
+				miniValue += seriesData[i].y;
+				miniLabel = miniLabel.concat(seriesData[i].name+": "+seriesData[i].y+"<br>");
+			} else {
+				passedValues.push(seriesData[i]);
+			}
+		};
+
+		if (i == 1) {
+			passedValues.sort(compare);
+			miniValue += passedValues[0].y;
+			miniLabel = miniLabel.concat(passedValues[0].name+": "+passedValues[0].y+"<br>");
+			passedValues.shift();
+		};
+
+		if (miniValue > 0) {
+			passedValues.push({
+				name: "Misc. ≤ " + (minThreshold*100).toFixed(1) +"%",
+				tooltip: miniLabel,
+				y: miniValue
+			});
+		};
+
+		return passedValues;
+	}
+
+	function compare(a,b) {
+		if (a.y < b.y)
+			return -1;
+		if (a.y > b.y)
+			return 1;
+		return 0;
 	}
 
 });
