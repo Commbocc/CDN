077bd2398d1fa72d39209a2b00d7f34fe338f03e
Kevin McCarthy <kjmagic13@gmail.com>
2015-07-06 10:18:16 -0400

    blue-ribbon nav js

---
+++ js/blue-ribbon-nav.js
@@ -0,0 +1,13 @@
+$(function () {
+
+	var navigation = '<ul class="nav nav-pills nav-justified">';
+	navigation += '<li class="active"><a data-toggle="tab" href="#services" aria-expanded="true">Model of Services</a></li>';
+	navigation += '<li><a href="#res">Resources</a></li>';
+	navigation += '<li><a href="http://www.hillsboroughcdms.org/blue-ribbon/child-fatalities">Facts &amp; Figures</a></li>';
+	navigation += '<li><a href="http://www.hillsboroughcdms.org/blue-ribbon/partners">Community Partners</a></li>';
+	navigation += '<li><a href="http://www.hillsboroughcdms.org/blue-ribbon/members">Members</a></li>';
+	navigation += '</ul>';
+
+	$('#blueRibbonNav').html(navigation);
+
+});
