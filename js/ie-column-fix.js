fb4749dfff56bc9bad566f571bf0687c44cb49b4
Kevin McCarthy <kjmagic13@gmail.com>
2015-07-09 11:38:02 -0400

    possible ie 8 column fix (offsets)

--- js/ie-column-fix.js
+++ js/ie-column-fix.js
@@ -13,4 +13,17 @@ $(function () {
 	$('.col-sm-11, .col-md-11, .col-lg-11').addClass('col-xs-11');
 	$('.col-sm-12, .col-md-12, .col-lg-12').addClass('col-xs-12');
 
+	$('.col-sm-offset-1, .col-md-offset-1, .col-lg-offset-1').addClass('col-xs-offset-1');
+	$('.col-sm-offset-2, .col-md-offset-2, .col-lg-offset-2').addClass('col-xs-offset-2');
+	$('.col-sm-offset-3, .col-md-offset-3, .col-lg-offset-3').addClass('col-xs-offset-3');
+	$('.col-sm-offset-4, .col-md-offset-4, .col-lg-offset-4').addClass('col-xs-offset-4');
+	$('.col-sm-offset-5, .col-md-offset-5, .col-lg-offset-5').addClass('col-xs-offset-5');
+	$('.col-sm-offset-6, .col-md-offset-6, .col-lg-offset-6').addClass('col-xs-offset-6');
+	$('.col-sm-offset-7, .col-md-offset-7, .col-lg-offset-7').addClass('col-xs-offset-7');
+	$('.col-sm-offset-8, .col-md-offset-8, .col-lg-offset-8').addClass('col-xs-offset-8');
+	$('.col-sm-offset-9, .col-md-offset-9, .col-lg-offset-9').addClass('col-xs-offset-9');
+	$('.col-sm-offset-10, .col-md-offset-10, .col-lg-offset-10').addClass('col-xs-offset-10');
+	$('.col-sm-offset-11, .col-md-offset-11, .col-lg-offset-11').addClass('col-xs-offset-11');
+	$('.col-sm-offset-12, .col-md-offset-12, .col-lg-offset-12').addClass('col-xs-offset-12');
+
 });
