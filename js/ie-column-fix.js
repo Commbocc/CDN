10b578e4290ce3421e9c224dd6614a60d29fc633
Kevin McCarthy <kjmagic13@gmail.com>
2015-07-15 07:51:06 -0400

    DRYed ie column fix

--- js/ie-column-fix.js
+++ js/ie-column-fix.js
@@ -1,29 +1,8 @@
 $(function () {
 
-	$('.col-sm-1, .col-md-1, .col-lg-1').addClass('col-xs-1');
-	$('.col-sm-2, .col-md-2, .col-lg-2').addClass('col-xs-2');
-	$('.col-sm-3, .col-md-3, .col-lg-3').addClass('col-xs-3');
-	$('.col-sm-4, .col-md-4, .col-lg-4').addClass('col-xs-4');
-	$('.col-sm-5, .col-md-5, .col-lg-5').addClass('col-xs-5');
-	$('.col-sm-6, .col-md-6, .col-lg-6').addClass('col-xs-6');
-	$('.col-sm-7, .col-md-7, .col-lg-7').addClass('col-xs-7');
-	$('.col-sm-8, .col-md-8, .col-lg-8').addClass('col-xs-8');
-	$('.col-sm-9, .col-md-9, .col-lg-9').addClass('col-xs-9');
-	$('.col-sm-10, .col-md-10, .col-lg-10').addClass('col-xs-10');
-	$('.col-sm-11, .col-md-11, .col-lg-11').addClass('col-xs-11');
-	$('.col-sm-12, .col-md-12, .col-lg-12').addClass('col-xs-12');
-
-	$('.col-sm-offset-1, .col-md-offset-1, .col-lg-offset-1').addClass('col-xs-offset-1');
-	$('.col-sm-offset-2, .col-md-offset-2, .col-lg-offset-2').addClass('col-xs-offset-2');
-	$('.col-sm-offset-3, .col-md-offset-3, .col-lg-offset-3').addClass('col-xs-offset-3');
-	$('.col-sm-offset-4, .col-md-offset-4, .col-lg-offset-4').addClass('col-xs-offset-4');
-	$('.col-sm-offset-5, .col-md-offset-5, .col-lg-offset-5').addClass('col-xs-offset-5');
-	$('.col-sm-offset-6, .col-md-offset-6, .col-lg-offset-6').addClass('col-xs-offset-6');
-	$('.col-sm-offset-7, .col-md-offset-7, .col-lg-offset-7').addClass('col-xs-offset-7');
-	$('.col-sm-offset-8, .col-md-offset-8, .col-lg-offset-8').addClass('col-xs-offset-8');
-	$('.col-sm-offset-9, .col-md-offset-9, .col-lg-offset-9').addClass('col-xs-offset-9');
-	$('.col-sm-offset-10, .col-md-offset-10, .col-lg-offset-10').addClass('col-xs-offset-10');
-	$('.col-sm-offset-11, .col-md-offset-11, .col-lg-offset-11').addClass('col-xs-offset-11');
-	$('.col-sm-offset-12, .col-md-offset-12, .col-lg-offset-12').addClass('col-xs-offset-12');
+	for (var i = 1; i <= 12; i++) {
+		$('.col-sm-'+i+', .col-md-'+i+', .col-lg-'+i).addClass('col-xs-'+i);
+		$('.col-sm-offset-'+i+', .col-md-offset-'+i+', .col-lg-offset-'+i).addClass('col-xs-offset-'+i);
+	}
 
 });
