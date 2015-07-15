606000aff2bd982ac96033870145824978b51320
Kevin McCarthy <kjmagic13@gmail.com>
2015-07-15 07:58:00 -0400

    DRYed ie column fix

--- js/ie-column-fix.js
+++ js/ie-column-fix.js
@@ -1,8 +1,20 @@
 $(function () {
 
-	for (var i = 1; i <= 12; i++) {
-		$('.col-sm-'+i+', .col-md-'+i+', .col-lg-'+i).addClass('col-xs-'+i);
-		$('.col-sm-offset-'+i+', .col-md-offset-'+i+', .col-lg-offset-'+i).addClass('col-xs-offset-'+i);
+	function IsIE8Browser() {
+		var rv = -1;
+		var ua = navigator.userAgent;
+		var re = new RegExp("Trident\/([0-9]{1,}[\.0-9]{0,})");
+		if (re.exec(ua) != null) {
+			rv = parseFloat(RegExp.$1);
+		}
+		return (rv == 4);
+	}
+
+	if (IsIE8Browser()) {
+		for (var i = 1; i <= 12; i++) {
+			$('.col-sm-'+i+', .col-md-'+i+', .col-lg-'+i).addClass('col-xs-'+i);
+			$('.col-sm-offset-'+i+', .col-md-offset-'+i+', .col-lg-offset-'+i).addClass('col-xs-offset-'+i);
+		}
 	}
 
 });
